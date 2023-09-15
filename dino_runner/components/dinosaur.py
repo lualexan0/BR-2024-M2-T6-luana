0444d909d5bd98ce34b8b44d2de057dbb3bd139f
Luana Alexandre <129203783+lualexan0@users.noreply.github.com>
2023-09-14 22:17:22 -0300

    aula_4_Luana
    Finalmente consegui rodar o jogo, obrigada pela ajuda professora

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -20,6 +20,7 @@ class Dinosaur(Sprite):
         self.dino_run = True
         self.dino_jump = False
         self.dino_duck = False
+
     def update(self, user_input):
         if self.dino_run:
             self.run()
@@ -32,3 +33,43 @@ class Dinosaur(Sprite):
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_jump = False 
+            self.dino_duck = True
+        elif not self.dino_jump and not self.dino_duck:
+            self.dino_run = True
+            self.dino_jump = False
+            self.dino_duck = False
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
+        self.step_index += 1
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+
+        if self.jump_vel < -JUMP_VEL:
+            self.dino_rect.y = Y_POS
+            self.dino_jump = False
+            self.jump_vel = JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
+        self.dino_duck = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
